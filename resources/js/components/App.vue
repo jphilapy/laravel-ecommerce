e4d3707684e04e99fc0ddac20e4742305ecd2e27
Jeff Philapy <jeff@altahost.com>
2022-01-23 18:04:34 -0500

    video 9 of facebook clone and tdd

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,19 +1,31 @@
 <template>
-<div class="d-flex flex-column vh-100 overflow-hidden">
-<Nav></Nav>
-<router-view></router-view>
-</div>
-</template>
-<script>
-import Nav from './Nav';
-export default {
-    name: "App",
+    <div class="flex flex-col flex-1 h-screen overflow-y-hidden">
+        <Nav />
 
-    components: {
-        Nav
+        <div class="flex overflow-y-hidden flex-1">
+            <Sidebar />
+
+            <div class="overflow-x-hidden w-2/3">
+                <router-view></router-view>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Nav from './Nav';
+    import Sidebar from './Sidebar';
+
+    export default {
+        name: "App",
+
+        components: {
+            Nav,
+            Sidebar
+        }
     }
-}
 </script>
+
 <style scoped>
 
 </style>
