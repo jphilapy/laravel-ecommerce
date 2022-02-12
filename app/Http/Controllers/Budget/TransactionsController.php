96ad043adef6a62d3fa73af1d1dfea6e52570040
Jeff Philapy <jeff@altahost.com>
2022-02-12 13:58:48 -0500

    misc

--- app/Http/Controllers/Budget/TransactionsController.php
+++ app/Http/Controllers/Budget/TransactionsController.php
@@ -8,6 +8,11 @@ use Illuminate\Http\Request;
 
 class TransactionsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         $transactions = Transaction::all();
--- tests/Feature/budget/ViewTransactionsTest.php
+++ tests/Feature/budget/ViewTransactionsTest.php
@@ -3,12 +3,25 @@
 namespace Tests\Feature\budget;
 
 use App\Models\Budget\Transaction;
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ViewTransactionsTest extends TestCase
 {
     use RefreshDatabase;
+    /**
+     * @test
+     */
+    public function it_allows_only_authenticated_users()
+    {
+//        $user = factory(User::class)->create();
+//        ->actingAs($user)
+
+        $this->get('/budget/transactions')
+            ->assertRedirect('/login');
+    }
+
     /**
      * @test
      */
