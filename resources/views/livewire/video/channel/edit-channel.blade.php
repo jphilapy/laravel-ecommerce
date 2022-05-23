4b8094bd7430ca450942408f5fec75beb2fad2ef
Jeff Philapy <jeff@altahost.com>
2022-05-22 21:58:02 -0400

    Video 8

--- resources/views/livewire/video/channel/edit-channel.blade.php
+++ resources/views/livewire/video/channel/edit-channel.blade.php
@@ -1,4 +1,20 @@
 <div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
-    {{$channel->name}}
+    <form wire:submit.prevent="update">
+        <div class="form-group">
+            <label for="name">Name</label>
+            <input type="text" id="name" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="slug">Slug</label>
+            <input type="text" id="slug" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="description">Description</label>
+            <textarea id="description" cols="30" rows="4" class="form-control"></textarea>
+        </div>
+
+        <div class="form-group">
+           <submit type="submit" class="btn btn-success">Update</submit>
+        </div>
+    </form>
 </div>
