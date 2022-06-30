7a8a75832da1c25d63433a840621e26a36ccd4b9
Jeff Philapy <jeff@altahost.com>
2022-06-29 22:08:38 -0400

    adding livewire components

---
+++ app/Http/Livewire/Video/AllVideos.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Video;
+
+use Livewire\Component;
+
+class AllVideos extends Component
+{
+    public function render()
+    {
+        return view('livewire.video.all-videos');
+    }
+}
