8b255dac219861c9798fc6e162dedf85c8a6553a
Jeff Philapy <jeff@altahost.com>
2022-06-30 23:07:03 -0400

    Video 18

--- app/Http/Livewire/Video/EditVideo.php
+++ app/Http/Livewire/Video/EditVideo.php
@@ -2,13 +2,44 @@
 
 namespace App\Http\Livewire\Video;
 
+use App\Models\Video\Channel;
+use App\Models\Video\Video;
 use Livewire\Component;
 
 class EditVideo extends Component
 {
+
+    public Channel $channel;
+    public Video $video;
+
+    protected $rules = [
+        'video.title' => 'required|max:255',
+        'video.description' => 'nullable|max:10000',
+        'video.visibility' => 'required|in:private,public,unlisted',
+    ];
+
+    public function mount($channel, $video)
+    {
+        $this->channel = $channel;
+        $this->video = $video;
+    }
+
     public function render()
     {
         return view('livewire.video.edit-video')
             ->extends('layouts.app');
+    }
+
+    public function update()
+    {
+        $this->validate();
+
+        $this->video->update([
+           'title'=>$this->video->title,
+           'description'=>$this->video->description,
+           'visibility'=>$this->video->visibility,
+        ]);
+
+        session()->flash('message', 'video was updated');
     }
 }
--- resources/views/livewire/video/edit-video.blade.php
+++ resources/views/livewire/video/edit-video.blade.php
@@ -1,3 +1,61 @@
 <div>
-    {{-- The best athlete wants his opponent at his best. --}}
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+
+                    <div class="card-body">
+
+                        <form wire:submit.prevent="update">
+                            <div class="form-group">
+                                <label for="title">Title</label>
+                                <input type="text" id="title" class="form-control" wire:model="video.title">
+                            </div>
+                            @error('video.title')
+                            <div class="alert alert-danger">
+                                {{$message}}
+                            </div>
+                            @enderror
+
+                            <div class="form-group">
+                                <label for="description">Description</label>
+                                <textarea id="description" cols="30" rows="4" class="form-control"
+                                          wire:model="video.description"></textarea>
+                            </div>
+                            @error('video.description')
+                            <div class="alert alert-danger">
+                                {{$message}}
+                            </div>
+                            @enderror
+
+                            <div class="form-group">
+                                <label for="visibility">Visibility</label>
+                                <select wire:model="video.visibility" class="form-control">
+                                    <option value="private">Private</option>
+                                    <option value="public">Public</option>
+                                    <option value="unlisted">Unlisted</option>
+                                </select>
+                            </div>
+
+                            @error('video.visibility')
+                            <div class="alert alert-danger">
+                                {{$message}}
+                            </div>
+                            @enderror
+
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-success">Update</button>
+                            </div>
+
+                            @if(session()->has('message'))
+                                <div class="alert alert-success">
+                                    {{session('message')}}
+                                </div>
+                            @endif
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
