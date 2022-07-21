ac98422ceb6acc803184660593bf6a353091aa26
Jeff Philapy <jeff@altahost.com>
2022-07-20 23:19:58 -0400

    Updating template code

--- resources/views/budget/categories/create.blade.php
+++ resources/views/budget/categories/create.blade.php
@@ -1,6 +1,9 @@
-@extends('layouts/app')
-
-@section('content')
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
     <div class="container">
         <div class="row">
             <div class="col-md-8 offset-md-2">
@@ -18,4 +21,4 @@
         </div>
 
     </div>
-@endsection
+    </x-app-layout>
